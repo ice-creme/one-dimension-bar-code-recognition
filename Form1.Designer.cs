1356540c386f7687b74050caf07cf495cd78984e
ice-creme <93702529+ice-creme@users.noreply.github.com>
2022-02-11 07:52:44 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace 一维码识别
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hWindowControl1 = new HalconDotNet.HWindowControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // hWindowControl1
+            // 
+            this.hWindowControl1.BackColor = System.Drawing.Color.White;
+            this.hWindowControl1.BorderColor = System.Drawing.Color.White;
+            this.hWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl1.Location = new System.Drawing.Point(12, 12);
+            this.hWindowControl1.Name = "hWindowControl1";
+            this.hWindowControl1.Size = new System.Drawing.Size(500, 387);
+            this.hWindowControl1.TabIndex = 0;
+            this.hWindowControl1.WindowSize = new System.Drawing.Size(500, 387);
+            this.hWindowControl1.HMouseMove += new HalconDotNet.HMouseEventHandler(this.hWindowControl1_HMouseMove);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(567, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 69);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "采集图象";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(567, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "处理显示图象";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hWindowControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private HalconDotNet.HWindowControl hWindowControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HalconDotNet;
+
+namespace 一维码识别
+{ 
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        HObject ho_image, ho_Region;
+        HTuple hv_WindowHandle = null, hv_BarCodeHandle = null;
+        HTuple hv_DecodedDataStrings = null, hv_Row1 = null, hv_Column1 = null;
+        HTuple hv_Row2 = null, hv_Column2 = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Imagepath;
+            //图象选取
+            openFileDialog1.Filter = "JPEG文件|*.jpg*|BMP文件|*.bmp*|PNG文件|*.png*";
+            openFileDialog1.RestoreDirectory = true;
+            HOperatorSet.GenEmptyObj(out ho_image);
+            HOperatorSet.GenEmptyObj(out ho_Region);
+            ho_image.Dispose();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Imagepath = openFileDialog1.FileName;
+                HOperatorSet.ReadImage(out ho_image, Imagepath);
+
+            }
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void hWindowControl1_HMouseMove(object sender, HMouseEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            HOperatorSet.OpenWindow(0, 0, hWindowControl1.Width,hWindowControl1.Height,hWindowControl1.HalconWindow, "visible", "", out hv_WindowHandle);
+            HDevWindowStack.Push(hv_WindowHandle);
+            //创建条码模型
+            HOperatorSet.CreateBarCodeModel(new HTuple(), new HTuple(), out hv_BarCodeHandle);
+            ho_Region.Dispose();
+            HOperatorSet.FindBarCode(ho_image, out ho_Region, hv_BarCodeHandle,"auto", out hv_DecodedDataStrings);
+            HOperatorSet.SmallestRectangle1(ho_Region, out hv_Row1, out hv_Column1,out hv_Row2, out hv_Column2);
+            //显示
+            if (HDevWindowStack.IsOpen())
+            {
+                HOperatorSet.SetDraw(HDevWindowStack.GetActive(), "margin");
+            }
+            if (HDevWindowStack.IsOpen())
+            {
+                HOperatorSet.SetLineWidth(HDevWindowStack.GetActive(), 3);
+            }
+            if (HDevWindowStack.IsOpen())
+            {
+                HOperatorSet.DispObj(ho_image, HDevWindowStack.GetActive());
+            }
+            if (HDevWindowStack.IsOpen())
+            {
+                HOperatorSet.DispObj(ho_Region, HDevWindowStack.GetActive());
+            }
+            if (HDevWindowStack.IsOpen())
+            {
+                HOperatorSet.DispText(HDevWindowStack.GetActive(), hv_DecodedDataStrings, "image",hv_Row2, hv_Column1, "black", new HTuple(), new HTuple());
+            }
+            //清除模型
+            HOperatorSet.ClearBarCodeModel(hv_BarCodeHandle);
+            ho_image.Dispose();
+            ho_Region.Dispose();
+
+        }
+
+    }
+}
